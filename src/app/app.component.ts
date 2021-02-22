44a44769416db8a85055bf250f307691b8e544cc
suvadip_gosh <suvadip.gosh@mheducation.com>
2021-02-22 17:36:35 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CI/CD Experiment for Angular';
+  description = 'This is angular code deployment using CI/CD';
+}
